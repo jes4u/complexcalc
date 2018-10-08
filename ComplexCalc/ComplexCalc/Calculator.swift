a5081e539ba6af7fd2badc88428db2a11fdd4e81
iosdev <iosdev@Felixs-13-MBP.local>
2018-10-07 21:53:01 -0700

    Everything excpet points

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,86 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    
+    //Add
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs;
+    }
+    
+    func add(_ list: Array<Int> ) -> Int {
+        var total = 0
+        for num in list {
+            total += num;
+        }
+        return total;
+        
+    }
+    
+    //Subtract
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs;
+    }
+    
+    func subtract(_ list: Array<Int> ) -> Int {
+        var total = 0
+        for num in list {
+            total -= num;
+        }
+        return total;
+        
+    }
+    
+    //Multiply
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs;
+    }
+    
+    func multiply(_ list: Array<Int> ) -> Int {
+        var total = 0
+        for num in list {
+            total -= num;
+        }
+        return total;
+        
+    }
+    
+    //Divide
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func divide(_ list: Array<Int> ) -> Int {
+        var total = 0
+        for num in list {
+            total /= num
+        }
+        return total
+        
+    }
+    
+    //Count
+    func count(_ list: Array<Int>) -> Int {
+        return list.count
+    }
+    
+    //Average
+    func avg(_ list: Array<Int>) -> Int {
+        var total = self.add(list)
+        total /= list.count
+        return total
+    }
+    
+    //MathOP
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int{
+        return op(lhs, rhs);
+    }
+    
+    func mathOp(args: Array<Int>, beg: Int, op: (Int, Int) -> Int) -> Int {
+        var total = 0;
+        for index in 0...args.count {
+            total += op(args[index], beg);
+        }
+        
+        return total;
+    }
 }
